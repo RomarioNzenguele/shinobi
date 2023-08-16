a5ff9d0d85508eb6b76293d71f31e8e46205e18b
“Romário <nz.romariodasilva@gmail.com>
2023-08-16 21:11:43 +0100

    Updating 'ChangeVar' folder struture & adding a python solution

---
+++ changevar/App.py
@@ -0,0 +1,9 @@
+#Declaracao e atribuicao 
+x = input('x: ')
+y = input('y: ')
+
+#Troca dos valores
+x, y = y, x
+
+#Imprimindo o resultado
+print("\n**** Valores trocados\nx: " + x + "\ny: " + y+"\n")
